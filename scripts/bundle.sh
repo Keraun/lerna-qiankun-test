d29a7b93a88e9f0ea1dad837f231eac4d3e6e2f8
laowu <wuly93@163.com>
2025-02-27 17:37:08 +0800

    feat: init

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -6,14 +6,14 @@ mkdir ./dist
 mkdir ./dist/subapp
 
 # 遍历子应用构建产物目录
-for app in $(ls ./packages); do
-  cp -rf ./packages/$app/dist ./dist/subapp/${app}/
+for app in $(ls ./app); do
+  cp -rf ./app/$app/dist ./dist/subapp/${app}/
 done
 
 rm -rf ./dist/subapp/mainapp
 
 # main基座
-cp -rf ./packages/mainapp/dist/ ./dist/
+cp -rf ./app/mainapp/dist/ ./dist/
 
 # cd ..
 echo 'bundle.sh execute success.'
